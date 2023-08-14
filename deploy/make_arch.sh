ebc14bf46dc5d8d4f36ee1656a4be7403f1bc0ea
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-08-14 17:03:08 +0200

    Modify deploy scripts

--- deploy/make_arch.sh
+++ deploy/make_arch.sh
@@ -1,7 +1,8 @@
 current_branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-mkdir IFS="_" read -ra parts <<< "$current_branch"
-target_dir = "../{$current_branch}/Lesson_{$parts[1]}_Bryukhovskikh"
+IFS="_" read -ra parts <<< "$current_branch"
+cd ../${current_branch}/
+target_dir="Lesson_${parts[1]}_Bryukhovskikh"
 mkdir $target_dir
-cp -r src/* $target_dir
+cp -r ../gb_patterns/src/* $target_dir
 find $target_dir -type d -name "__pycache__" -exec rm -r {} \;
-zip -r ../{$current_branch}/Lesson_{$parts[1]}_Bryukhovskikh.zip $target_branch
+zip -r ${target_dir}.zip $target_dir
